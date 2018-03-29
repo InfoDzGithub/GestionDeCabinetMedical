db72670fb92849689957fffcab2508e6b8cbd166
LatifaInfoDz <bouchekiflatifa13@gmail.com>
2018-03-29 15:44:26 +0100

    Add button ADD to manage Appointement

--- src/javafx/ManageAppointmentController.java
+++ src/javafx/ManageAppointmentController.java
@@ -6,13 +6,24 @@
 package javafx;
 
 import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -22,10 +33,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 /**
  * FXML Controller class
@@ -40,12 +57,114 @@ public class ManageAppointmentController implements Initializable {
     private JFXDatePicker SelectTimer;
     @FXML
     private ComboBox<String> combobox;
-     @FXML
-    private JFXTextField firstName_box;
     @FXML
-    private JFXTextField familyName_box;
+    private JFXTextArea comment_box;
+    
+    
+    
+   
+    ObservableList<String> list2 = FXCollections.observableArrayList();
+     private Connexion db;
+    ObservableList<Rdv> list = FXCollections.observableArrayList();
     @FXML
-     ObservableList<String> list = FXCollections.observableArrayList();
+    private TableView<Rdv> tab;
+    @FXML
+    private TableColumn<Rdv, Integer> id_col;
+    @FXML
+    private TableColumn<Rdv, String> pat_inf_col;
+    @FXML
+    private TableColumn<Rdv, Date> date_col;
+    @FXML
+    private TableColumn<Rdv, Time> time_col;
+    @FXML
+    private TableColumn<Rdv, String> comment_col;
+    
+ /*************************************************************************************************************/ 
+    @FXML
+    public void insertData(ActionEvent event) { 
+        
+       
+      String date=dateSelector.getValue().toString();
+      String time=SelectTimer.getTime().toString();
+     // String commT =comment_box.getText().replaceAll("\n", System.getProperty("line.separator"));
+     String text =comment_box.getText();
+     String patient_inf=combobox.selectionModelProperty().getValue().getSelectedItem();
+    
+         if(date.isEmpty() || time.isEmpty() || text.isEmpty() || patient_inf.isEmpty())
+         
+         {
+             infoBox2("Please Fill Out The Form ", null, "Form Error!");  
+         }
+       
+        String sql="INSERT INTO Rdv(date_rdv,heure_rdv,info_P,commentaire) VALUES(?,?,?,?)";
+       Connection conn;
+                   try {
+                  conn=Connexion.ConnecrDB();
+                  PreparedStatement preparedSt=conn.prepareStatement(sql);
+                 
+                  preparedSt.setString(1,date);
+                  preparedSt.setString(2, time);
+                  preparedSt.setString(3, patient_inf);
+                  preparedSt.setString(4, text);
+                   
+                  
+                  preparedSt.execute();
+                  infoBox("RDV Added Successfully", null, "Success");
+                   loadData();
+                 
+                  SelectTimer.setTime(LocalTime.of(14, 0));
+                  dateSelector.setValue(LocalDate.of(2018, Month.MARCH, 29));
+                  comment_box.clear();
+                  combobox.getSelectionModel().clearSelection();
+                   
+                 // dateSelector.getEditor().clear();
+                  
+                   
+                    } 
+                   catch (Exception e)
+                        {
+                            
+                        }     
+  
+        
+         }
+    
+    
+ /**************************************************************************************************************/
+    
+     public void loadData(){
+       list.clear();
+         try {
+             Connection con=Connexion.ConnecrDB();
+           
+            ResultSet rs = con.createStatement().executeQuery("SELECT * FROM rdv");
+            while (rs.next()) {
+                //get string from db,whichever way 
+                list.add(new Rdv(rs.getString(1),rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5) ));
+            }
+
+        } catch (Exception ex) {
+            System.err.println("Error"+ex);
+        }
+   
+        id_col.setCellValueFactory(new PropertyValueFactory<Rdv,Integer>("ID"));
+   
+      pat_inf_col.setCellValueFactory(new PropertyValueFactory<Rdv,String>("infP"));
+      comment_col.setCellValueFactory(new PropertyValueFactory<Rdv,String>("comment"));
+      date_col.setCellValueFactory(new PropertyValueFactory<Rdv,Date>("date"));
+      time_col.setCellValueFactory(new PropertyValueFactory<Rdv,Time>("time"));
+        
+     
+     tab.setItems(list);
+  
+}
+  
+ /**************************************************************************************************************/
+    
+    
+    
+    
+    
     
  /*****************************************************************************************************************/
      @FXML
@@ -64,15 +183,15 @@ public class ManageAppointmentController implements Initializable {
             window.setScene(ab);
             window.show(); } 
  /******************************************************************************************************************/   
- public void loadData(){
-       list.clear();
+ public void loadData2(){
+       list2.clear();
          try {
              Connection con=Connexion.ConnecrDB();
            
             ResultSet rs = con.createStatement().executeQuery("SELECT concat(concat(nom_pat,' '),concat(prenom_pat,' '),nic_pat) FROM patient");
             while (rs.next()) {
                 //get string from db,whichever way 
-                list.add(new String(rs.getString(1)));
+                list2.add(new String(rs.getString(1)));
             }
 
         } catch (Exception ex) {
@@ -84,15 +203,20 @@ public class ManageAppointmentController implements Initializable {
  /*****************************************************************************************************************/   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-     loadData();
-     combobox.setItems(list);
+     loadData2();
+     combobox.setItems(list2);
+     //SelectTimer.setTime(LocalTime.of(14, 0));
+     //dateSelector.setValue(LocalDate.of(2018, 03, 28));
+      loadData();
+    
+    
  }
 /******************************************************************************************************************/
     @FXML
     private void ComboboxSelect(MouseEvent event) {
        
-        String id=combobox.getSelectionModel().getSelectedItem();
-       String red="SELECT * from patient where concat(concat(nom_pat,' '),concat(prenom_pat,' '),nic_pat)='" +id+"'";
+    /*    String id=combobox.getSelectionModel().getSelectedItem();
+       String red="SELECT * from patient where concat(concat(nom_pat,'  '),concat(prenom_pat,'   '),nic_pat)='" +id+"'";
     try{
                Connection conn=Connexion.ConnecrDB();
                PreparedStatement preparedSt=conn.prepareStatement(red);
@@ -109,7 +233,77 @@ public class ManageAppointmentController implements Initializable {
    catch(Exception e){
        
    }
+   */
     }  
         
  /******************************************************************************************************************/   
+
+ public static class Rdv{
+    
+     private final SimpleStringProperty  ID,infP,date,time,comment;
+    // private final DateFormat date;
+    
+    //constructeur
+    public Rdv(String ID,String date,String time,String infP,String comment)
+    {//
+      this.ID = new SimpleStringProperty(ID);
+      this.infP = new SimpleStringProperty(infP);
+      //this.date= new SimpleDateFormat("yy/mm/jj");
+      this.time = new SimpleStringProperty(time);
+      this.date = new SimpleStringProperty(date);
+      this.comment= new SimpleStringProperty(comment);
+      //SimpleDateFormat d = new SimpleDateFormat ("dd/MM/yyyy" );
+      //SimpleDateFormat h = new SimpleDateFormat ("hh:mm");  
+            
+    }
+    
+     public String getID() {
+            return ID.get();
+        }
+
+   
+         public String getInfP() {
+            return infP.get();
+        }
+
+        public String getDate() {
+            return  date.get();
+        }
+
+        public  String  getTime() {
+            return time.get();
+        }
+
+       public  String  getComment() {
+            return comment.get();
+       }
+ }
+/**************************************************************************************************************/  
+    
+  public static void infoBox(String infoMsg,String headerText,String title)         
+       {
+           Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
+           alert.setContentText(infoMsg);
+           alert.setTitle(title);
+           alert.setHeaderText(headerText);
+           alert.showAndWait();
+           
+       }    
+ /**************************************************************************************************************/   
+     public static void infoBox2(String infoMsg,String headerText,String title)         
+       {
+           Alert alert=new Alert(Alert.AlertType.ERROR);
+           alert.setContentText(infoMsg);
+           alert.setTitle(title);
+           alert.setHeaderText(headerText);
+           alert.showAndWait();
+           
+       }    
+    
+    
+ /******************************************************************************************************************/  
+       
+        
+    
+
 }
